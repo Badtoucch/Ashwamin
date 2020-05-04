0279c21622399c8f4a87abd33a1f1be70d387a3b
Badtoucch <Badtoucch>
2020-05-04 16:08:46 +0300

    vlad

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,10 @@
 import "./helpers/postDate";
 import scrollSmooth from "./helpers/smooth-scroll.js";
 // import example from './modules/example.js';
-import infection from "./modules/infection.js";
-import people from "./modules/people.js";
 import footer from "./modules/footer.js";
 
 function main() {
 	footer();
-	people();
-	infection();
 }
 
 main();
